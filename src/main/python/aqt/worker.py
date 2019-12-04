cc23c95bb3b0a19f5f0a24e2d793963733ed8a01
Jacob Clarke <jacobclarke718@gmail.com>
2019-12-04 15:56:05 -0600

    updated to use pgrep to find running apps

--- src/main/python/aqt/worker.py
+++ src/main/python/aqt/worker.py
@@ -5,7 +5,7 @@ import subprocess
 from aqt.closer import Closer
 import signal
 import os
-from gi.repository import Wnck
+import pgrep
 
 
 class Worker(QRunnable):
@@ -18,35 +18,30 @@ class Worker(QRunnable):
         self.app = appctxt.app
         self.closer = Closer(self.appctxt)
         self.timer = Screentime(appctxt)
-        self.screen = Wnck.Screen.get_default()
-        self.screen.force_update()
+        self.logger = self.appctxt.logger
 
     @pyqtSlot()
     def run(self):
         while True:
             self.block_apps()
+            self.logger.info("running worker")
             time.sleep(10)
-            # make sure that tray is showing
             # TODO increase increment based on which is closest to rnning out
             # TODO finish making tray work on startup
 
     def block_apps(self):
         """ closes blocked apps """
         blocked = self.timer.apply_limits()
-        app_list = [x.get_application() for x in self.screen.get_windows()]
-        for app in app_list:
-            if blocked.app.str.lower().isin([app.get_name().lower()]).any():
-                # self.closer.set_warning(row.title, row.time_limit)
-
-                row = blocked[blocked.app.str.lower().isin([app.get_name().lower()])]
-                limit = (row.time_limit.iloc[0])
-                self.closer.set_warning(app.get_name(), limit)
-                self.appctxt.logger.info(f"Sending warning for {app.get_name()}")
+        self.logger.debug(blocked)
+        for index, row in blocked.iterrows():
+            instance = pgrep.pgrep(row.app)
+            if instance:
+                self.closer.set_warning(row.app, row.time_limit)
+                # self.appctxt.logger.info(f"Sending warning for {row.app}")
                 response = self.closer.exec_()
                 if response == 1:
-                    self.appctxt.logger.info(f"Killed {app.get_name()}")
+                    self.appctxt.logger.info(f"Killed {row.app}")
                     subprocess.call(['notify-send',
-                                    f'Closing {app.get_name()}. Time limit reached.'])
-                    os.kill(app.get_pid(), signal.SIGKILL)
-                # elif response == 2:
-                #     self.timer.increase_limit(title)
+                                    f'Closing {row.app}. Time limit reached.'])
+                    for pid in instance:
+                        os.kill(pid, signal.SIGKILL)
--- src/main/python/utils/timer.py
+++ src/main/python/utils/timer.py
@@ -55,29 +55,5 @@ class Screentime():
         df_groups = df_groups[df_groups.time_limit <= df_groups.duration]
         return df_groups
 
-#     def increase_limit(self, group_name):
-#         query = f"""
-# SELECT id  FROM limit_group WHERE title = {group_name}
-# """
-#         group_id = [x for x, in self.con.execute(query)][0]
-
-#         query = f"""
-# SELECT MAX(time_limit) from limit_increase where limit_group_id = {group_id}
-# """
-#         max = [x for x, in self.con.execute(query)][0]
-
-#         # increase max
-#         new_limit = 15 + max
-#         today = datetime.now()
-
-#         query = f"""
-# INSERT INTO limit_increase (day, time_limit, limit_group_id)
-# VALUES ('{today}', {new_limit}, {group_id})
-# """
-#         self.con.execute(query)
-#         self.con.commit()
-#         self.appctxt.logger.info(f"Adding 15 minutes to {group_name}."
-#               f"The new limit is {new_limit} min.")
-#         self.appctxt.db.update_config()
 # TODO add ability to increase time limit
 
